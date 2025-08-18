9d2526d4f1ed4180a6d23797787bf676399b34c1
valli2506 <2400030238@kluniversity.in>
2025-08-18 10:43:52 +0530

    Initial commit-added JS practice code

---
+++ Javascript/Example1.js
@@ -0,0 +1,22 @@
+var city='London';
+let country='UK';
+const pi=3.14;
+console.log(country)
+console.log(city)
+
+let a1=100;
+let a2=200;
+console.log(a1+a2);
+
+// let sum=5+3;
+// let isEqual=(sum===8);
+// console.log(isEqual);
+
+let attendence=20+5;
+let eligibleForExam=(attendence>=75);
+console.log("Eligible for exAM?",eligibleForExam);
+
+function greetStudent(name)
+{
+    return "welcome" +name
+}
---
+++ Javascript/Example3.js
@@ -0,0 +1,36 @@
+// // let today=new Date();
+// // console.log("Date:",today);
+
+//  let students=["Valli","Dhruti","Lasya"];
+// // students.push("Aarshu");
+// // console.log(students)
+// // students.unshift("Karthik");
+// // console.log(students)
+// // students.splice(2,1,"Venkat");
+// // console.log(students)
+// // students.pop("Valli")
+// // console.log(students)
+
+// for(let student of students){
+//     console.log(student);
+// }
+
+// console.log("while loop-roll numbers");
+// let rn=101;
+// while(rn<=103){
+//     console.log("Roll no:",rn);
+//     rn++;
+// }
+
+// console.log("Do...while loop-hostel floors");
+// let floor=1;
+// do{
+//     console.log("hostel floor:",floor);
+//     floor++;
+// }while(floor<=3);
+
+let studentMap=new Map();
+studentMap.set("101",{name:"valli",dept:"CSE"});
+studentMap.set("102",{name:"jaya",dept:"ECE"});
+studentMap.set("103",{name:"chandu",dept:"Commerce"});
+console.log("Student name:",studentMap.get(""));
